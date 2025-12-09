6ff168111a68968a6a137dcc9abaabf90e391169
Ananya M <ananyamalleswamy@gmail.com>
2025-12-09 15:44:28 +0000

    this is testing my patience

--- firstfile.c
+++ firstfile.c
@@ -4,6 +4,7 @@ int main(){
 
     printf("hello dadigli ananyaaa\n");
     printf("hello maha dadigli shreyaaa!\n");
+    printf("i'm adding this to check whether this will work, oh my god im done already!!")
     int x=10;
     printf("%d",x);
     return 0;
