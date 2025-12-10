eae70f5c112b138b81796ef9b747c98a1968266f
shreyaganeshiyer <shreyaganeshiyer@gmail.com>
2025-12-10 13:17:11 +0530

    coloured text

--- firstfile.c
+++ firstfile.c
@@ -2,10 +2,11 @@
 
 int main(){
 
-    printf("hello dadigli ananyaaa\n");
+   /* printf("hello dadigli ananyaaa\n");
     printf("hello maha dadigli shreyaaa!\n");
     printf("i'm adding this to check whether this will work, oh my god im done already!!");
     int x=10;
-    printf("%d",x);
+    printf("%d\n",x);*/
+   printf("\033[3;35;46mhi my name is shreya \033[0m\n");
     return 0;
 }
