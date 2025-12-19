d899c5ae8a7eddf9860ce4fae81ef9184b823732
shreyaganeshiyer <shreyaganeshiyer@gmail.com>
2025-12-19 19:26:51 +0530

    add

---
+++ src/main.c
@@ -0,0 +1,19 @@
+#include "raylib.h"
+#include<stdlib.h>
+
+
+int main(){
+InitWindow(50*12,50*12,"Easylevel");
+
+while(!WindowShouldClose()){
+
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+
+
+    EndDrawing();
+}
+
+
+CloseWindow();
+}
