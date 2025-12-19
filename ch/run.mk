478541dc79e7e9941fce071c5f61ad2872217b51
shreyaganeshiyer <shreyaganeshiyer@gmail.com>
2025-12-19 20:45:50 +0530

    added stuff

---
+++ ch/run.mk
@@ -0,0 +1,20 @@
+CC = gcc
+CFLAGS = -Wall -IE:/raylib-5.5_win64_mingw-w64/raylib-5.5_win64_mingw-w64/include
+LIBS = -LE:/raylib-5.5_win64_mingw-w64/raylib-5.5_win64_mingw-w64/lib -lraylib -lopengl32 -lgdi32 -lwinmm
+
+# default output = a.exe
+all:
+    $(CC) rayy.c $(CFLAGS) $(LIBS)
+
+# output as filename.exe
+named:
+    $(CC) rayy.c -o rayy.exe $(CFLAGS) $(LIBS)
+
+run:
+    ./a.exe
+
+run_named:
+    ./rayy.exe
+
+clean:
+    del a.exe rayy.exe
--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include "raylib.h"
 #include<stdlib.h>
 
+int size=12;
 
 int main(){
 InitWindow(50*12,50*12,"Easylevel");
@@ -10,6 +11,11 @@ while(!WindowShouldClose()){
     BeginDrawing();
     ClearBackground(RAYWHITE);
 
+for(int ver=0;ver<12;ver++){
+    for(int i=0;i<12;i++){
+        DrawRectangle(i*50,ver*50,48,48,BLUE);
+    }
+}
 
     EndDrawing();
 }
