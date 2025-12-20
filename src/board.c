33ba578f94a2ad68f4d7ca52e416571c9bedd173
shreyaganeshiyer <shreyaganeshiyer@gmail.com>
2025-12-20 15:47:28 +0530

    .

--- src/board.c
+++ src/board.c
@@ -1,8 +1,98 @@
 #include<stdio.h>
+#include "raylib.h"
 
+int size = 12;
 
 int main(){
+    Vector2 click;
+    int x = -1, y = -1;              // for left click highlight
+    int valx = -1, valy = -1;        // temp for right click
+    bool flagarr[12][12] = {false};  // store flags
+    bool highlight[12][12]={false};
 
-    printf("hi minesweeper");
-    return 0;
-}
+// -------------------BOMB ASSISNMENT----------------------
+    srand(time(NULL)); // seed once at start of the game
+    bool isMine[12][12]={false};  // it is bool type lol
+
+    int count=15;
+    int placed=0;
+
+    InitWindow(50*12, 50*12, "Easylevel");
+
+    while(placed<count){
+        int r=rand()%12;
+        int c=rand()%12;
+        if(!isMine[r][c]){  
+            isMine[r][c]=true;
+            placed++;
+        }
+
+    }
+
+    Image img = LoadImage("1f6a9.png");
+    ImageResize(&img, 48, 48);
+    Texture2D flage = LoadTextureFromImage(img);
+    UnloadImage(img);
+
+    Image bom = LoadImage("BOMB.png"); ///cpu
+    ImageResize(&bom, 48, 48);
+    Texture2D bomb_tex = LoadTextureFromImage(bom);
+    UnloadImage(bom);
+
+    while(!WindowShouldClose()){
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+
+        // drawing the grid
+        for(int ver = 0; ver < 12; ver++){
+            for(int i = 0; i < 12; i++){
+                DrawRectangle(i*50, ver*50, 48, 48, BLUE);
+
+                if(flagarr[ver][i] == true){
+                    DrawTexture(flage, i*50, ver*50, WHITE);
+                }
+
+                if(highlight[ver][i]==true){
+                DrawRectangle(i*50,ver*50, 48,48, PINK);
+                
+                 if(isMine[ver][i]==true){
+                 DrawTexture(bomb_tex, i*50, ver*50, WHITE);
+                }
+                 }
+
+               
+        }
+    }
+
+        //-------------------------- left click highlight--------------------------------
+        if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
+            click = GetMousePosition();
+            int col = click.x / 50;
+            int row = click.y / 50;
+
+            highlight[row][col]=true;
+            x = col * 50;
+            y = row * 50;
+
+        }
+
+        //------------------------- right click toggle flag----------------------
+        if(IsMouseButtonPressed(MOUSE_BUTTON_RIGHT)){
+            click = GetMousePosition();
+            int col = click.x / 50;
+            int row = click.y / 50;
+            valx = col * 50;
+            valy = row * 50;
+
+            if(col >= 0 && col < 12 && row >= 0 && row < 12){
+                flagarr[row][col] = !flagarr[row][col]; // true->false or false->true
+                //initially false
+            }
+        }
+        
+        EndDrawing();
+    }
+        
+    UnloadTexture(flage);
+    CloseWindow();
+    }
--- src/main.c
+++ src/main.c
@@ -1,15 +1,86 @@
 #include "raylib.h"
+#include "assets.h"
+
 #include<stdlib.h>
 #include<stdbool.h>
 #include<time.h>
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 800
+#define TILE_HEIGHT 50
+#define TILE_WIDTH 50
 int size = 12;
 
 int main(){
     InitWindow(SCREEN_WIDTH,SCREEN_HEIGHT,"Minesweeper");
     SetTargetFPS(60);
+    while(!WindowShouldClose()){
+        GameUpdate();
+    }
     return 0;
 }
 
+typedef enum{
+    STATE_MAIN_MENU,
+    STATE_OPTIONS_MENU,
+    STATE_PLAYING,
+    STATE_LOSE,
+    STATE_WIN
+}GameState;
+void GameUpdate(){
+    
+void GameUpdate() {
+    UpdateMusicStream(music[MUSIC_ONE]);
 
+    switch (gameState) {
+    case STATE_MAIN_MENU:
+        if (IsKeyPressed(KEY_N)) {
+            GamePlaySound(SOUND_TWO);
+            GameReset();
+        } else if (IsKeyPressed(KEY_O)) {
+            gameState = STATE_OPTIONS_MENU;
+            GamePlaySound(SOUND_TWO);
+        }
+        break;
+    case STATE_OPTIONS_MENU:
+        if (IsKeyPressed(KEY_ENTER)) {
+            gameState = STATE_MAIN_MENU;
+            GamePlaySound(SOUND_TWO);
+        }
+        if (IsKeyPressed(KEY_S)) {
+            isSoundEnabled = !isSoundEnabled;
+            PlaySound(sounds[SOUND_ONE]);
+        }
+        if (IsKeyPressed(KEY_M)) {
+            isMusicEnabled = !isMusicEnabled;
+            PlaySound(sounds[SOUND_ONE]);
+            if (isMusicEnabled) {
+                StopMusicStream(music[MUSIC_ONE]);
+                PlayMusicStream(music[MUSIC_ONE]);
+            } else {
+                StopMusicStream(music[MUSIC_ONE]);
+            }
+        }
+        break;
+    case STATE_PLAYING:
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+            Vector2 mp = GetMousePosition();
+            int col = (int)(mp.x / TILE_WIDTH);
+            int row = (int)(mp.y / TILE_HEIGHT);
+            if (IsTileIndexValid(col, row)) {
+                RevealTile(grid[col][row]);
+                GamePlaySound(SOUND_ONE);
+            }
+        } else if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT)) {
+            Vector2 mp = GetMousePosition();
+            int col = (int)(mp.x / TILE_WIDTH);
+            int row = (int)(mp.y / TILE_HEIGHT);
+            if (IsTileIndexValid(col, row)) {
+                FlagTile(grid[col][row]);
+            }
+
+        }
+
+    }
+}
+
+}
